fe763b401fe5ed1a463f8bd224381cc88194fc04
LosmanDev <losman78@gmail.com>
2022-05-18 17:38:25 -0400

    updates

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 $(".port-item").click(function () {
   $(".collapse").collapse("hide");
 });
+
+//A lightbox gallery plugin for Bootstrap 3 based on the modal plugin
+$(document).on("click", '[data-toggle="lightbox"]', function (e) {
+  e.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+// BOOTSTRAP MODAL FOR READ ME
+$("#myModal").on("shown.bs.modal", function () {
+  $("#myInput").trigger("focus");
+});
