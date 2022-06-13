76b2465d2b5472194a10b6afc4e42d07c1339bbd
LosmanDev <losman78@gmail.com>
2022-06-13 12:59:53 -0400

    updates

--- script.js
+++ script.js
@@ -31,6 +31,30 @@ function sendMail(params) {
     .send("service_n1w0hcr", "template_lngukbq", tempParams)
     .then(function (res) {
       console.log("Success", res.status);
-      location.reload();
+      e.preventDefault();
     });
 }
+
+// MESSAGE SENT DISCLAIMER FOR FORM
+let buttonVanish = document.getElementById("form-vanish");
+let contact_form = document.getElementById("form-hidden");
+
+buttonVanish.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  contact_form.hidden = true;
+
+  contact_form.innerHTML = `
+    <div class="container mt-4"" data-aos="fade-right" data-aos-duration="2000">
+    <h1 class="display-4 text-center font-weight-bold">
+      Message Sent! <i class="fa-solid fa-envelope-circle-check"></i>
+    </h1>
+  </div>
+    `;
+
+  contact_form.hidden = false;
+
+  setTimeout(() => {
+    location.reload();
+  }, 5000);
+});
